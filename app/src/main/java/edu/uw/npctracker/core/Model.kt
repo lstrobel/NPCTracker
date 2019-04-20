a0aecc721565e6a0f7dbe40833b89952c48f74f9
Owen DeArmond-MacLeod <owendm@uw.edu>
2019-04-20 01:28:48 -0700

    Create Model Interface

---
+++ app/src/main/java/edu/uw/npctracker/core/Model.kt
@@ -0,0 +1,29 @@
+package edu.uw.npctracker.core
+
+/*
+ * @author owendm
+ * A model manages generated characters
+ */
+interface Model {
+
+    /**
+     * @return The number of characters currently in the model
+     */
+    fun size(): Int
+
+    /**
+     * @param character the character to add to the model, if is already in model does nothing
+     */
+    fun plusCharacter(character: Character)
+
+    /**
+     * @param character the character to remove to the model
+     */
+    fun removeCharacter(character: Character)
+
+    /**
+     * @return The list of characters currently in the model
+     */
+    fun characters(): List<Character>
+
+}

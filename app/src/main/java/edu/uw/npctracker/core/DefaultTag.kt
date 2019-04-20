19a0d02b9c340abaf6c1b90ad56f9f17e526c8ac
lstrobel <lstrobel66@gmail.com>
2019-04-20 13:05:06 -0700

    Add equals and hashCode for impl

--- app/src/main/java/edu/uw/npctracker/core/DefaultTag.kt
+++ app/src/main/java/edu/uw/npctracker/core/DefaultTag.kt
@@ -4,4 +4,19 @@ package edu.uw.npctracker.core
 /*
  * @author lstrobel
  */
-class DefaultTag(override val label: String): Tag {}
+class DefaultTag(override val label: String) : Tag {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as DefaultTag
+
+        if (label != other.label) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return label.hashCode()
+    }
+}

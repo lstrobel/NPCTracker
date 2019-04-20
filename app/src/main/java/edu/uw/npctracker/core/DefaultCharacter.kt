8ecf76b07bdbffcb649d9e6394973d091590784d
lstrobel <lstrobel66@gmail.com>
2019-04-20 12:37:20 -0700

    Add Character and Tag implementation

---
+++ app/src/main/java/edu/uw/npctracker/core/DefaultCharacter.kt
@@ -0,0 +1,66 @@
+package edu.uw.npctracker.core
+
+import java.time.Instant
+
+/*
+ * @author lstrobel
+ */
+class DefaultCharacter private constructor(
+    private val firstName: String,
+    private val lastName: String,
+    private val notes: String,
+    private val tags: Set<Tag>
+) : Character {
+
+    private val timestamp = Instant.now()
+
+    constructor() : this("", "", "", HashSet())
+
+    override fun getFirstName(): String {
+        return firstName
+    }
+
+    override fun setFirstName(name: String): Character {
+        return DefaultCharacter(name, lastName, notes, tags)
+    }
+
+    override fun getLastName(): String {
+        return lastName
+    }
+
+    override fun setLastName(name: String): Character {
+        return DefaultCharacter(firstName, name, notes, tags)
+    }
+
+    override fun getNotes(): String {
+        return notes
+    }
+
+    override fun setNotes(text: String): Character {
+        return DefaultCharacter(firstName, lastName, text, tags)
+    }
+
+    override fun getTags(): Set<Tag> {
+        return tags
+    }
+
+    override fun addTag(tag: Tag): Character {
+        val newTags = HashSet(tags)
+        newTags.add(tag)
+        return DefaultCharacter(firstName, lastName, notes, newTags)
+    }
+
+    override fun removeTag(tag: Tag): Character {
+        val newTags = HashSet(tags)
+        newTags.remove(tag)
+        return DefaultCharacter(firstName, lastName, notes, newTags)
+    }
+
+    override fun hasTag(tag: Tag): Boolean {
+        return tags.contains(tag)
+    }
+
+    override fun getTimestamp(): Instant {
+        return timestamp
+    }
+}
---
+++ app/src/main/java/edu/uw/npctracker/core/DefaultTag.kt
@@ -0,0 +1,8 @@
+package edu.uw.npctracker.core
+
+
+/*
+ * Lukas Strobel Copyright 2019
+ */
+class DefaultTag(override val label: String): Tag {
+}
